f537c3c5a8ab8d610655cdc30d482bc60d90e1b9
Danila-alinaD <126895741+Danila-alinaD@users.noreply.github.com>
2026-01-03 21:21:00 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,59 +1,59 @@
 const products = [{id:1, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 1", desc:"Опис товару 1", material:"Матеріал", size:"Розмір", quantity:"100 шт", weight:"50 г", price:100},
-    ru:{name:"Товар 1", desc:"Описание товара 1", material:"Материал", size:"Размер", quantity:"100 шт", weight:"50 г", price:100}
+    uk:{name:"Розмальовка 1", desc:"Опис товару 1", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 1", desc:"Описание товара 1", material:"Материал", size:"Размер", quantity:"1 шт", weight:"200 г", price:100}
     },{id:2, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 2", desc:"Опис товару 2", material:"Матеріал", size:"Розмір", quantity:"200 шт", weight:"100 г", price:200},
-    ru:{name:"Товар 2", desc:"Описание товара 2", material:"Материал", size:"Размер", quantity:"200 шт", weight:"100 г", price:200}
+    uk:{name:"Розмальовка 2", desc:"Опис товару 2", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 2", desc:"Описание товара 2", material:"Материал", size:"120x150", quantity:"1 шт", weight:"200 г", price:200}
     },{id:3, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 3", desc:"Опис товару 3", material:"Матеріал", size:"Розмір", quantity:"150 шт", weight:"75 г", price:300},
-    ru:{name:"Товар 3", desc:"Описание товара 3", material:"Материал", size:"Размер", quantity:"150 шт", weight:"75 г", price:300}
+    uk:{name:"Розмальовка 3", desc:"Опис товару 3", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 3", desc:"Описание товара 3", material:"Материал", size:"Размер", quantity:"1 шт", weight:"75 г", price:300}
     },{id:4, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 4", desc:"Опис товару 4", material:"Матеріал", size:"Розмір", quantity:"120 шт", weight:"60 г", price:400},
-    ru:{name:"Товар 4", desc:"Описание товара 4", material:"Материал", size:"Размер", quantity:"120 шт", weight:"60 г", price:400}
+    uk:{name:"Розмальовка 4", desc:"Опис товару 4", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 4", desc:"Описание товара 4", material:"Материал", size:"Размер", quantity:"1 шт", weight:"60 г", price:400}
     },{id:5, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 5", desc:"Опис товару 5", material:"Матеріал", size:"Розмір", quantity:"180 шт", weight:"90 г", price:500},
-    ru:{name:"Товар 5", desc:"Описание товара 5", material:"Материал", size:"Размер", quantity:"180 шт", weight:"90 г", price:500}
+    uk:{name:"Розмальовка 5", desc:"Опис товару 5", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 5", desc:"Описание товара 5", material:"Материал", size:"Размер", quantity:"1 шт", weight:"90 г", price:500}
     },{id:6, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 6", desc:"Опис товару 6", material:"Матеріал", size:"Розмір", quantity:"250 шт", weight:"125 г", price:600},
-    ru:{name:"Товар 6", desc:"Описание товара 6", material:"Материал", size:"Размер", quantity:"250 шт", weight:"125 г", price:600}
+    uk:{name:"Розмальовка 6", desc:"Опис товару 6", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 6", desc:"Описание товара 6", material:"Материал", size:"Размер", quantity:"1 шт", weight:"125 г", price:600}
     },{id:7, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 7", desc:"Опис товару 7", material:"Матеріал", size:"Розмір", quantity:"300 шт", weight:"150 г", price:700},
-    ru:{name:"Товар 7", desc:"Описание товара 7", material:"Материал", size:"Размер", quantity:"300 шт", weight:"150 г", price:700}
+    uk:{name:"Розмальовка 7", desc:"Опис товару 7", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 7", desc:"Описание товара 7", material:"Материал", size:"Размер", quantity:"1 шт", weight:"150 г", price:700}
     },{id:8, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 8", desc:"Опис товару 8", material:"Матеріал", size:"Розмір", quantity:"110 шт", weight:"55 г", price:800},
-    ru:{name:"Товар 8", desc:"Описание товара 8", material:"Материал", size:"Размер", quantity:"110 шт", weight:"55 г", price:800}
+    uk:{name:"Розмальовка 8", desc:"Опис товару 8", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 8", desc:"Описание товара 8", material:"Материал", size:"Размер", quantity:"1 шт", weight:"55 г", price:800}
     },{id:9, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 9", desc:"Опис товару 9", material:"Матеріал", size:"Розмір", quantity:"160 шт", weight:"80 г", price:900},
-    ru:{name:"Товар 9", desc:"Описание товара 9", material:"Материал", size:"Размер", quantity:"160 шт", weight:"80 г", price:900}
+    uk:{name:"Розмальовка 9", desc:"Опис товару 9", material:"Папір", size:"120x150", quantity:"1 шт", weight:"200 г", price:220},
+    ru:{name:"Товар 9", desc:"Описание товара 9", material:"Материал", size:"Размер", quantity:"1 шт", weight:"80 г", price:900}
     },{id:10, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 10", desc:"Опис товару 10", material:"Матеріал", size:"Розмір", quantity:"140 шт", weight:"70 г", price:1000},
-    ru:{name:"Товар 10", desc:"Описание товара 10", material:"Материал", size:"Размер", quantity:"140 шт", weight:"70 г", price:1000}
+    uk:{name:"Розмальовка 10", desc:"Опис товару 10", material:"Папір", size:"120x150", quantity:"1 шт", weight:"70 г", price:220},
+    ru:{name:"Товар 10", desc:"Описание товара 10", material:"Материал", size:"Размер", quantity:"1 шт", weight:"70 г", price:1000}
     },{id:11, category:1,
-    images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 11", desc:"Опис товару 11", material:"Матеріал", size:"Розмір", quantity:"220 шт", weight:"110 г", price:1100},
-    ru:{name:"Товар 11", desc:"Описание товара 11", material:"Материал", size:"Размер", quantity:"220 шт", weight:"110 г", price:1100}
+    images:["img/stikersps_1.jpg","img/stikersps_1.jpg","img/stikersps_1.jpg"],
+    uk:{name:"Стікери Playstation Classic", desc:"Набір наліпок, натхненний атмосферою олдскульних ігор на PlayStation — зі швидкістю Ridge Racer, похмурим вайбом першого Killzone та футуристичною енергією Wipeout. Для тих, хто пам’ятає звук завантаження консолі й любов до епохи класичної PlayStation.", material:"ПВХ", size:"10.5х18.5", quantity:"1 шт", weight:"20 г", price:90},
+    ru:{name:"Стикеры Playstation Classic", desc:"Набор наклеек, вдохновленный атмосферой олдскульных игр на PlayStation — со скоростью Ridge Racer, мрачным вайбом первого Killzone и футуристической энергией Wipeout. Для тех, кто помнит звук загрузки консоли и любовь к эпохе классической PlayStation.", material:"ПВХ", size:"10.5х18.5", quantity:"1 шт", weight:"20 г", price:90}
     },{id:12, category:1,
-    images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 12", desc:"Опис товару 12", material:"Матеріал", size:"Розмір", quantity:"190 шт", weight:"95 г", price:1200},
-    ru:{name:"Товар 12", desc:"Описание товара 12", material:"Материал", size:"Размер", quantity:"190 шт", weight:"95 г", price:1200}
+    images:["img/stikerssweetvibes_1.jpg","img/stikerssweetvibes_1.jpg","img/stikerssweetvibes_1.jpg"],
+    uk:{name:"Стікери Sweet Vibes 6 шт", desc:"Стікери у ніжних рожевих тонах, створені для романтичного настрою та щоденного натхнення. Набір складається з 6 наліпок, які підійдуть для прикрашання щоденників, ноутбуків чи листівок і додадуть тепла у повсякденні дрібниці.", material:"ПВХ", size:"2x2", quantity:"6 шт", weight:"10 г", price:20},
+    ru:{name:"Стикеры Sweet Vibes 6 шт", desc:"Стикеры в мягких розовых тонах, созданные для романтического настроения и ежедневного вдохновения. Набор состоит из 6 наклеек, которые подойдут для украшения дневников, ноутбуков или листовки и добавят тепла в ежедневные мелочи.", material:"ПВХ", size:"2x2", quantity:"6 шт", weight:"10 г", price:20}
     },{id:13, category:1,
-    images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 13", desc:"Опис товару 13", material:"Матеріал", size:"Розмір", quantity:"130 шт", weight:"65 г", price:1300},
-    ru:{name:"Товар 13", desc:"Описание товара 13", material:"Материал", size:"Размер", quantity:"130 шт", weight:"65 г", price:1300}
+    images:["img/stikersfallout2_1.jpg","img/stikersfallout2_2.jpg","img/stikersfallout2_3.jpg"],
+    uk:{name:"Cтікери, що світяться Fallout 2 Neon", desc:"Набір яскравих наліпок, що світяться, у стилі гри Fallout 2. Наліпки з логотипами, символами та персонажами з культової постапокаліптичної рольової гри. Ідеально підійдуть для декору, прикрашення ноутбуків, телефонів та інших поверхонь.", material:"Люмінесцентна ПВХ", size:"21x10", quantity:"1 шт", weight:"65 г", price:350},
+    ru:{name:"Стикеры, что светятся Fallout 2 Neon", desc:"Набор ярких наклеек, что светится, в стиле игры Fallout 2. Наклейки с логотипами, символами и персонажами из культовой постапокалиптической ролевой игры. Идеально подойдут для декора, украшения ноутбуков, телефонов и других поверхностей.", material:"Люминесцентная ПВХ", size:"21x10", quantity:"1 шт", weight:"65 г", price:350}
     },{id:14, category:1,
-    images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
-    uk:{name:"Товар 14", desc:"Опис товару 14", material:"Матеріал", size:"Розмір", quantity:"170 шт", weight:"85 г", price:1400},
-    ru:{name:"Товар 14", desc:"Описание товара 14", material:"Материал", size:"Размер", quantity:"170 шт", weight:"85 г", price:1400}
+    images:["img/stikershikaru_1.jpg","img/stikershikaru_1.jpg","img/stikershikaru_1.jpg"],
+    uk:{name:"Стікери по аніме Літо, коли помер Хікару", desc:"Стікери за мотивами аніме «Літо, коли помер Хікару», виконані з легким, трохи веселим настроєм, що контрастує з оригінальною історією. Поєднання літньої атмосфери та м’якого гумору робить їх приємним акцентом для щоденних речей і фанатів аніме.", material:"ПВХ", size:"15х10", quantity:"1 шт", weight:"35 г", price:40},
+    ru:{name:"Стикеры по аниме Лето, когда умер Хикару", desc:"Стикеры за мотивами аниме «Лето, когда умер Хикару», выполненные с легким, немного веселым настроением, который контрастирует с оригинальной историей. Сочетание летней атмосферы и мягкого юмора делает их приятным акцентом для ежедневных вещей и фанатов аниме.", material:"ПВХ", size:"15х10", quantity:"1 шт", weight:"35 г", price:40}
     },{id:15, category:1,
     images:["img/p_uni.jpg.webp","img/p_uni.jpg.webp","img/p_uni.jpg.webp"],
     uk:{name:"Товар 15", desc:"Опис товару 15", material:"Матеріал", size:"Розмір", quantity:"210 шт", weight:"105 г", price:1500},
